be7c794b7336e79e56beeff6e620fe52700d45f1
cdaguanno <102310974+cdaguanno@users.noreply.github.com>
2022-06-14 17:45:45 -0400

    Update GetExternalIP.ps1

--- GetExternalIP.ps1
+++ GetExternalIP.ps1
@@ -1,1 +1,10 @@
 ﻿Invoke-RestMethod -Uri ('http://ipinfo.io/'+(Invoke-WebRequest -uri "http://ifconfig.me/ip").Content)
+ $logdir = "c:\GetIPPSlog"
+[String]$time = Get-Date
+$logLine = $time  
+if (Test-path -Path $logdir) {
+    $logLine | Out-File -Filepath $logdir\execLog.log -Append
+} ELSE {
+       mkdir "c:\GetIPPSlog"
+    $logLine | Out-File -Filepath $logdir\execLog.log
+}
